2ab73d1a11583c88e0d52a242893c21fd2846203
Gosia Heba <malgorzataheba@gmail.com>
2021-03-19 18:24:38 +0100

    Extract mocks into a dedicated file

--- src/utils/parse-log-file.test.ts
+++ src/utils/parse-log-file.test.ts
@@ -1,17 +1,12 @@
+import {
+  mockInvalidLog,
+  mockValidLog,
+  mockPartiallyValidLog,
+} from '../test-helpers';
 import { parseLogFile } from './parse-log-file';
 
 describe('parseLogFile()', () => {
   describe('with a valid log file', () => {
-    const mockLogFile = `
-      /help_page/1 126.318.035.038
-      /help_page/1 929.398.951.889
-      /help_page/1 929.398.951.889
-      /contact 184.123.665.067
-      /contact 184.123.665.067
-      /home 184.123.665.067
-      /home 929.398.951.889
-    `;
-
     const expected = new Map([
       [
         '/help_page/1',
@@ -40,30 +35,24 @@ describe('parseLogFile()', () => {
     ]);
 
     it('should return a number of unique and total visits per page', () => {
-      const result = parseLogFile(mockLogFile);
+      const result = parseLogFile(mockValidLog);
       expect(result.pageVisitsMap).toEqual(expected);
     });
 
     it('should return an empty invalidEntries array', () => {
-      const result = parseLogFile(mockLogFile);
+      const result = parseLogFile(mockValidLog);
       expect(result.invalidEntries).toEqual([]);
     });
   });
 
   describe('with an invalid log file', () => {
-    const invalidLog = `
-      abcdefgh
-      /home 99999999
-      home 123.123.123.123
-    `;
-
     it('should return an empty page visit Map', () => {
-      const result = parseLogFile(invalidLog);
+      const result = parseLogFile(mockInvalidLog);
       expect(result.pageVisitsMap).toEqual(new Map());
     });
 
     it('should return all invalid lines inside invalidEntry array', () => {
-      const result = parseLogFile(invalidLog);
+      const result = parseLogFile(mockInvalidLog);
       expect(result.invalidEntries).toEqual([
         'abcdefgh',
         '/home 99999999',
@@ -73,15 +62,8 @@ describe('parseLogFile()', () => {
   });
 
   describe('with a partially valid log file', () => {
-    const partiallyValidLog = `
-      /help_page/1 126.318.035.038
-      /help_page/1 929.398.951.889
-      /help_page/1 929.398.951.889
-      /home 99999999
-    `;
-
     it('should return a page visit Map containing all valid information', () => {
-      const result = parseLogFile(partiallyValidLog);
+      const result = parseLogFile(mockPartiallyValidLog);
       const expected = new Map([
         [
           '/help_page/1',
@@ -96,7 +78,7 @@ describe('parseLogFile()', () => {
     });
 
     it('should return all invalid lines inside invalidEntry array', () => {
-      const result = parseLogFile(partiallyValidLog);
+      const result = parseLogFile(mockPartiallyValidLog);
       expect(result.invalidEntries).toEqual(['/home 99999999']);
     });
   });
